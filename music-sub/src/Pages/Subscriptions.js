7295d30e184792c5213c916a507866905a2c8b1c
anthony-vo <s3951749@student.rmit.edu.au>
2025-04-10 01:24:17 +1000

    lambda function and helper function for image

--- music-sub/src/Pages/Subscriptions.js
+++ music-sub/src/Pages/Subscriptions.js
@@ -1,127 +1,161 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import {
-    Navbar,
-    Container,
-    Nav,
-    Card,
-    Button,
-    Row,
-    Col
+  Navbar,
+  Container,
+  Nav,
+  Card,
+  Button,
+  Row,
+  Col,
 } from "react-bootstrap";
-import { FaUser } from 'react-icons/fa';
+import { FaUser } from "react-icons/fa";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Sidebar from "../Components/Sidebar";
+import axios from "axios";
+import { convertSongsWithPresignedURL } from "../api/musicAPI";
+
+const BASE_API_URL =
+  "https://ziwqlob0vj.execute-api.us-east-1.amazonaws.com/Production";
 
 const SubscriptionsPage = () => {
-    const navigate = useNavigate();
-    const [currentUser, setCurrentUser] = useState(null);
-    const [songs, setSongs] = useState([]);
+  const navigate = useNavigate();
+  const [currentUser, setCurrentUser] = useState(null);
+  const [songs, setSongs] = useState([]);
 
-    useEffect(() => {
-        const user = JSON.parse(localStorage.getItem("currentUser"));
-        if (user) {
-            setCurrentUser(user);
-            setSongs(user.songs);  // Getting all user subscriptions
-        } else {
-            navigate("/login");
-        }
-    }, [navigate]);
+  //Hardcoded user for testing purpose
 
-    const handleRemove = (songTitle) => {
-        const updatedSongs = songs.filter((song) => song.title !== songTitle);
-        setSongs(updatedSongs);
-        const updatedUser = { ...currentUser, songs: updatedSongs };
-        setCurrentUser(updatedUser);
-        localStorage.setItem("currentUser", JSON.stringify(updatedUser));
+  const userEmail = "s39224062@student.rmit.edu.au";
 
-        const users = JSON.parse(localStorage.getItem("users")) || [];
-        const userIndex = users.findIndex((u) => u.email === currentUser.email);
-        if (userIndex !== -1) {
-            users[userIndex] = updatedUser;
-            localStorage.setItem("users", JSON.stringify(users));
-        }
-    };
-
-    const handleLogout = () => {
-        localStorage.removeItem("currentUser");
+  useEffect(() => {
+    axios
+      .get(
+        `${BASE_API_URL}/Task5LambdaGetUser/${encodeURIComponent(userEmail)}`
+      )
+      .then(async (res) => {
+        const user = res.data;
+        setCurrentUser(user);
+        const userSubscriptions = user.songs || [];
+        const updatedSongsImage = await convertSongsWithPresignedURL;
+        setSongs(updatedSongsImage);
+      })
+      .catch((err) => {
+        console.error("Error fetching data: ", err);
         navigate("/login");
-    };
+      });
+  }, [navigate, userEmail]);
 
-    return (
-        <div className="text-white" style={{
-            minHeight: "100vh",
-            background: "linear-gradient(to bottom, rgba(10, 10, 10, 0.9), rgba(0, 0, 0, 1))"
-        }}>
+  const handleRemove = (songTitle, songAlbum) => {
+    axios
+      .delete(
+        `${BASE_API_URL}/Task5LambdaRemoveSong/${encodeURIComponent(
+          currentUser.email
+        )}`,
+        {
+          data: { title: songTitle, album: songAlbum },
+        }
+      )
+      .then(async (res) => {
+        const updatedUser = res.data;
+      });
+  };
 
-            <div className="main-layout" style={{ display: "flex" }}>
-                <Sidebar />
-                <div className="content-area">
-                    <Navbar>
-                        <Container fluid>
-                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                            <Navbar.Collapse id="basic-navbar-nav">
-                                <Nav className="ms-auto">
-                                    {currentUser && (
-                                        <Nav.Link disabled style={{ color: 'white' }}>
-                                            <FaUser style={{ marginRight: '8px' }} />
-                                            Welcome, {currentUser.username}
-                                        </Nav.Link>
-                                    )}
-                                    <Nav.Link onClick={handleLogout} style={{ color: '#9e19dc', fontWeight: 'bold' }}>
-                                        Logout
-                                    </Nav.Link>
-                                </Nav>
-                            </Navbar.Collapse>
-                        </Container>
-                    </Navbar>
+  const handleLogout = () => {
+    localStorage.removeItem("currentUser");
+    navigate("/login");
+  };
 
-                    <Container style={{ marginTop: 0 }} className="recent-subs">
-                        <h2>Your Subscriptions</h2>
-                        <div style={{
-                            maxHeight: "calc(100vh - 150px)", // Make it scrollable
-                            overflowY: "auto",
-                            overflowX: "hidden"
-                        }}>
-                            {songs.length > 0 ? (
-                                <Row>
-                                    {songs.map((song, index) => (
-                                        <Col key={index} md={4} lg={2} className="mb-3">
-                                            <div className="flip-card">
-                                                <div className="flip-card-inner">
-                                                    <div className="flip-card-front">
-                                                        <Card.Img variant="top" src={song.img_url} alt={song.artist} />
-                                                    </div>
-                                                    <div className="flip-card-back">
-                                                        <Card.Body>
-                                                            <Card.Title>{song.title}</Card.Title>
-                                                            <Card.Text>
-                                                                <strong>Artist:</strong> {song.artist} <br />
-                                                                <strong>Album:</strong> {song.album} <br />
-                                                                <strong>Year:</strong> {song.year}
-                                                            </Card.Text>
-                                                            <Button variant="danger" onClick={() => handleRemove(song.title)}>
-                                                                Unsubscribe
-                                                            </Button>
-                                                        </Card.Body>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <p style={{ marginTop: -20, fontSize: 16 }}>{song.title}</p>
-                                        </Col>
-                                    ))}
-                                </Row>
-                            ) : (
-                                <p style={{ textAlign: 'center', marginTop: '20px' }}>
-                                    You don't have any subscriptions yet!
-                                </p>
-                            )}
+  return (
+    <div
+      className="text-white"
+      style={{
+        minHeight: "100vh",
+        background:
+          "linear-gradient(to bottom, rgba(10, 10, 10, 0.9), rgba(0, 0, 0, 1))",
+      }}
+    >
+      <div className="main-layout" style={{ display: "flex" }}>
+        <Sidebar />
+        <div className="content-area">
+          <Navbar>
+            <Container fluid>
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                  {currentUser && (
+                    <Nav.Link disabled style={{ color: "white" }}>
+                      <FaUser style={{ marginRight: "8px" }} />
+                      Welcome, {currentUser.username}
+                    </Nav.Link>
+                  )}
+                  <Nav.Link
+                    onClick={handleLogout}
+                    style={{ color: "#9e19dc", fontWeight: "bold" }}
+                  >
+                    Logout
+                  </Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+
+          <Container style={{ marginTop: 0 }} className="recent-subs">
+            <h2>Your Subscriptions</h2>
+            <div
+              style={{
+                maxHeight: "calc(100vh - 150px)", // Make it scrollable
+                overflowY: "auto",
+                overflowX: "hidden",
+              }}
+            >
+              {songs.length > 0 ? (
+                <Row>
+                  {songs.map((song, index) => (
+                    <Col key={index} md={4} lg={2} className="mb-3">
+                      <div className="flip-card">
+                        <div className="flip-card-inner">
+                          <div className="flip-card-front">
+                            <Card.Img
+                              variant="top"
+                              src={song.img_url}
+                              alt={song.artist}
+                            />
+                          </div>
+                          <div className="flip-card-back">
+                            <Card.Body>
+                              <Card.Title>{song.title}</Card.Title>
+                              <Card.Text>
+                                <strong>Artist:</strong> {song.artist} <br />
+                                <strong>Album:</strong> {song.album} <br />
+                                <strong>Year:</strong> {song.year}
+                              </Card.Text>
+                              <Button
+                                variant="danger"
+                                onClick={() => handleRemove(song.title)}
+                              >
+                                Unsubscribe
+                              </Button>
+                            </Card.Body>
+                          </div>
                         </div>
-                    </Container>
+                      </div>
+                      <p style={{ marginTop: -20, fontSize: 16 }}>
+                        {song.title}
+                      </p>
+                    </Col>
+                  ))}
+                </Row>
+              ) : (
+                <p style={{ textAlign: "center", marginTop: "20px" }}>
+                  You don't have any subscriptions yet!
+                </p>
+              )}
             </div>
-            </div>
+          </Container>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default SubscriptionsPage;
--- music-sub/src/api/musicAPI.js
+++ music-sub/src/api/musicAPI.js
@@ -1,27 +1,57 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'https://xpbtobv7s1.execute-api.us-east-1.amazonaws.com/user/music-app-login';
+const API_URL =
+  "https://xpbtobv7s1.execute-api.us-east-1.amazonaws.com/user/music-app-login";
+const GET_IMAGE_API =
+  "https://ziwqlob0vj.execute-api.us-east-1.amazonaws.com/Production/Task5LambdaGetImage";
 
 export const registerUser = async (email, password, user_name) => {
-    try {
-        const response = await axios.post(API_URL, {
-            type: 'post',
-            email,
-            password,
-            user_name
-        });
+  try {
+    const response = await axios.post(API_URL, {
+      type: "post",
+      email,
+      password,
+      user_name,
+    });
 
-        // Handle response
-        const data = response.data;
-        if (data.statusCode === 200) {
-            return data;
-        } else {
-            throw new Error(data.message || "Email already in use");
-        }
-
-    } catch (error) {
-        throw new Error(
-            error.response?.data?.message || error.message || "Something went wrong."
-        );
+    // Handle response
+    const data = response.data;
+    if (data.statusCode === 200) {
+      return data;
+    } else {
+      throw new Error(data.message || "Email already in use");
     }
+  } catch (error) {
+    throw new Error(
+      error.response?.data?.message || error.message || "Something went wrong."
+    );
+  }
+};
+
+// Function to build the objectKey
+const buildKey = (song) => {
+  return `${song.artist}-${song.album}-${song.title}`;
+};
+
+// Calling API for lambda function handling the generation of presigned URL
+export const generatePresignedURL = async (objectKey) => {
+  try {
+    const response = await axios.post(GET_IMAGE_API, { objectKey });
+    // Return from lambda is the presigned URL
+    return response.data.presignedURL;
+  } catch (error) {
+    console.error("Error in retrieving for: ", objectKey, error);
+    return objectKey;
+  }
+};
+
+// convert img_url in dynamoDB to presigned URL from S3
+export const convertSongsWithPresignedURL = async (songs) => {
+  return await Promise.all(
+    songs.map(async (song) => {
+      const objectKey = buildKey(song);
+      const presignedURL = await generatePresignedURL(objectKey);
+      return { ...song, img_url: presignedURL };
+    })
+  );
 };
